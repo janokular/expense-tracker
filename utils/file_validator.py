8f5e0d5445a7ad37c9683158fb08b0bfc8cd7958
janekokularczyk@gmail.com <janekokularczyk@gmail.com>
2025-05-26 20:05:22 +0200

    staged changes

--- utils/file_validator.py
+++ utils/file_validator.py
@@ -5,4 +5,6 @@ import csv
 def validate_file(csv_file):
     '''Check if expenses.csv exisits'''
     if not os.path.exists(csv_file):
-        pass
+       # Create an epmty CSV file
+        with open (csv_file, 'w') as file:
+            pass
