7a43907b986a0bb9f051a6000a3b8d2eaa057719
janokular <janekokularczyk@gmail.com>
2025-06-03 15:45:59 +0200

    staged changes

--- src/expenses.py
+++ src/expenses.py
@@ -1,10 +1,24 @@
 import csv
+import datetime
 from enums.MonthsEnum import MonthsEnum
 
 
 def add(description, amount, file):
-    print(description)
-    print(amount)
+    '''Add new expense'''
+    id = 1
+
+    with open(file, 'r', newline='') as csv_file:
+        expense_reader = csv.reader(csv_file)
+        id += sum(1 for row in expense_reader)
+
+    with open(file, 'a', newline='') as csv_file:
+        expense_writer = csv.writer(csv_file)
+        expense_writer.writerow([id,
+                                 description,
+                                 amount,
+                                 datetime.date.today().month])
+
+    print(f'Expense added successfully (ID: {id})')
 
 
 def update(id, description, amount, file):
