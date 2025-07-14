d03e419477c7a188a89c78ceaa65938cfb3f8b6f
Jan <janekokularczyk@gmail.com>
2025-07-14 23:44:18 +0200

    staged changes

--- src/update_expens.py
+++ src/update_expens.py
@@ -1,9 +1,11 @@
 import csv
+from utils.id_checker import is_id_in_range
 
 
 def update_expense(id, description, amount, file):
     '''Update expense's description or amount'''
-    print(description)
-    print(amount)
+    if is_id_in_range(id, file):
+        print(description)
+        print(amount)
 
-    print(f'Expense {id} updated successfully')
+        print(f'Expense {id} updated successfully')
