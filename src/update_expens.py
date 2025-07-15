da2d4473db776d138e5905c9a44c054d73a82345
janokular <janekokularczyk@gmail.com>
2025-07-15 14:24:13 +0200

    updated update_expens.py

--- src/update_expens.py
+++ src/update_expens.py
@@ -5,7 +5,17 @@ from utils.id_checker import is_id_in_range
 def update_expense(id, description, amount, file):
     '''Update expense's description or amount'''
     if is_id_in_range(id, file):
-        print(description)
-        print(amount)
+        with open(file, 'r', newline='') as csv_expenses:
+            expenses = [expense for expense in csv.reader(csv_expenses)]
+
+            for expense in expenses:
+                if id == int(expense[0]):
+                    if description:
+                        expense[2] = description
+                    if amount:
+                        expense[3] = amount
+
+        with open(file, 'w', newline='') as csv_expenses:
+            csv.writer(csv_expenses).writerows(expenses)
 
         print(f'Expense {id} updated successfully')
