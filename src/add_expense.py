786104502e7179fb82d310f4aa5cd1a332088d58
janokular <janekokularczyk@gmail.com>
2025-07-15 17:05:50 +0200

    updated add_expense.py

--- src/add_expense.py
+++ src/add_expense.py
@@ -8,7 +8,8 @@ def add_expense(description, amount, file):
     date = get_current_date()
 
     with open(file, 'r', newline='') as csv_expenses:
-        id += sum(1 for expense in csv.reader(csv_expenses))
+        for expense in csv.reader(csv_expenses):
+            id += 1
 
     with open(file, 'a', newline='') as csv_expenses:
         csv.writer(csv_expenses).writerow([id,
