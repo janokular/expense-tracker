975c1457bc557bceb533a0984068a72d4d0e6f7e
janokular <janekokularczyk@gmail.com>
2025-07-22 16:04:41 +0200

    updated list_expenses.py

--- src/list_expenses.py
+++ src/list_expenses.py
@@ -1,8 +1,16 @@
 import csv
-
+from utils.table_print import table_print
 
 def list_expenses(file):
     '''List all expenses'''
+    expenses = []
+
     with open(file, 'r', newline='') as csv_expenses:
         for expense in csv.reader(csv_expenses):
-            print(f'{expense[0]} {expense[1]} {expense[2]} ${expense[3]}')
+            expenses.append([expense[0],
+                            expense[1],
+                            expense[2],
+                            f'${expense[3]}'])
+    
+    if expenses:
+        table_print(expenses)
