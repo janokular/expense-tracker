1f36abfa6b1f6ec9da440f95578eb3e09beb304b
Jan <janekokularczyk@gmail.com>
2026-02-04 22:28:58 +0100

    staged changes

--- expense_tracker/core/print_service.py
+++ expense_tracker/core/print_service.py
@@ -8,28 +8,28 @@ def column_print(expenses: list):
     ]
     expenses.insert(0, HEADER)
 
-    id_col_len = 0
-    date_col_len = 0
-    description_col_len = 0
-    amount_col_len = 0
+    id_spacing = 0
+    date_spacing = 0
+    desc_spacing = 0
+    amount_spacing = 0
 
     for e in expenses:
-        if id_col_len < len(e[0]):
-            id_col_len = len(e[0])
+        if id_spacing < len(e[0]):
+            id_spacing = len(e[0])
         
-        if date_col_len < len(e[1]):
-            date_col_len = len(e[1])
+        if date_spacing < len(e[1]):
+            date_spacing = len(e[1])
 
-        if description_col_len < len(e[2]):
-            description_col_len = len(e[2])
+        if desc_spacing < len(e[2]):
+            desc_spacing = len(e[2])
         
-        if amount_col_len < len(e[3]):
-            amount_col_len = len(e[3])
+        if amount_spacing < len(e[3]):
+            amount_spacing = len(e[3])
 
     for e in expenses:
         print(
-            f'{e[0]} {' ' * (id_col_len - len(e[0]))}',
-            f'{e[1]} {' ' * (date_col_len - len(e[1]))}',
-            f'{e[2]} {' ' * (description_col_len - len(e[2]))}',
-            f'{e[3]} {' ' * (amount_col_len - len(e[3]))}'
+            f'{e[0]} {' ' * (id_spacing - len(e[0]))}',
+            f'{e[1]} {' ' * (date_spacing - len(e[1]))}',
+            f'{e[2]} {' ' * (desc_spacing - len(e[2]))}',
+            f'{e[3]} {' ' * (amount_spacing - len(e[3]))}'
         )
